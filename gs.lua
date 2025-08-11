a0f9db06b6b80e0b0778dec3fa5f60b48c8b298d
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-08-11 20:52:12 +0700

    Create gs.lua

---
+++ gs.lua
@@ -0,0 +1,32 @@
+setfpscap(2)
+getgenv().GINVENTORY_COUNTS = {}
+getgenv().GBLOCK_PARTY_RAFFLE_ENTRIES = 0
+getgenv().GBLOCK_PARTY_STOP_REBIRTHING_ON_TIER = 99
+getgenv().GBLOCK_PARTY_STOP_REBIRTHING_ON_TITANIC = true
+getgenv().GBLOCK_PARTY_STOP_REBIRTHING_ON_MULTIPLIER2 = 50
+getgenv().GDO_BLOCK_PARTY = true
+getgenv().GCOLLECT_VENDING_MACHINES = true
+getgenv().GCOLLECT_FREE_ITEMS = true
+getgenv().GRANK_FIRST = true
+getgenv().GZONE_TO = 99
+getgenv().GREBIRTH_TO = 2
+getgenv().GRANK_TO = 2
+getgenv().GGFX_MODE = 1
+getgenv().GWEBHOOK_USERID = "698853568291143821"
+getgenv().GWEBHOOK_LINK = "https://discord.com/api/webhooks/1283496015286112306/6GynCAV9qP7lv4kCSkAZ7RY37TbpqkZROFwoHmvVtIgq4QYLywx9oDyg1rEy-6fkkAKI"
+getgenv().GHUGE_COUNT = 0
+getgenv().GMAIL_RECEIVERS = {"DreamSoCow"}
+getgenv().GMAIL_ITEMS = {
+    ["All Huges"] = {Class = "Pet", Id = "All Huges", MinAmount = 1},
+    ["Send Diamonds"] = {Class = "Currency", Id = "Diamonds", KeepAmount = "5m", MinAmount = "30m"},
+    ["Summer Block Party Tic ket"] = {Class = "Misc", Id = "Summer Block Party Ticket", MinAmount = 50},
+    ["Summer Gift"] = {Class = "Lootbox", Id = "Summer Gift", MinAmount = 50},
+    ["Hype Egg 2"] = {Class = "Lootbox", Id = "Hype Egg 2", MinAmount = 1},
+    ["Daycare egg 5"] = {Class = "Egg", Id = "Huge Machine Egg 5", MinAmount = 1},
+    ["Secret pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
+    ["Secret pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
+    ["Secret pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
+    ["Secret pet4"] = {Class = "Pet", Id = "Yellow Lucky Block", MinAmount = 1, AllVariants = true},
+}
+getgenv().GMAX_MAIL_COST = "2m"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
