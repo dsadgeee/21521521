7d1f1d028b44daad373adbc07953ffb563408bf5
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-08-12 21:00:34 +0700

    Update a.lua

--- a.lua
+++ a.lua
@@ -33,7 +33,7 @@ getgenv().gagConfig = {
     FAVOURITE_FRUIT_MUTATIONS = {},  -- Stop Autosell
     SKIP_HARVEST_MUTATIONS = {},  -- Stop Harvest
 
-    KEEP_PETS = { "Lobster Thermidor", ["Gorilla Chef"] = 8, "Sunny-Side Chicken", "Hotdog Daschund", ["Bacon Pig"] = 4, "Junkbot", "French Fry Ferret", "Spaghetti Sloth", ["Mochi Mouse"] = 2, ["Kodama"] = 1, "Corrupted Kitsune", ["Starfish"] = 12, ["Capybara"] = 1, ["Tanchozuru"] = 1, ["Seal"] = 1, "Kitsune", "Blood Kiwi", "Disco Bee", "Raccoon", "Queen Bee", "Dragonfly", "Butterfly", "Mimic Octopus", "Red Fox" },
+    KEEP_PETS = { "Lobster Thermidor", ["Gorilla Chef"] = 8, "Sunny-Side Chicken", "Hotdog Daschund", ["Bacon Pig"] = 4, "Junkbot", "French Fry Ferret", "Spaghetti Sloth", ["Mochi Mouse"] = 2, ["Kodama"] = 1, "Corrupted Kitsune", ["Starfish"] = 5, ["Capybara"] = 1, ["Tanchozuru"] = 1, ["Seal"] = 1, "Kitsune", "Blood Kiwi", "Disco Bee", "Raccoon", "Queen Bee", "Dragonfly", "Butterfly", "Mimic Octopus", "Red Fox" },
     KEEP_PETS_WEIGHT = 6,
 
     -- EQUIP_PETS Priority (Left -> Right)
