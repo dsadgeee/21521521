870ba114e0e12067412595bd8c3c1f289b9a44a3
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-08-13 18:07:48 +0700

    Update AYA.lua

--- AYA.lua
+++ AYA.lua
@@ -1,95 +1,107 @@
 wait(5)
 setfpscap(2)
 getgenv().ConfigsKaitun = {
-	Beta_Fix_Data_Sync = true,
+    Beta_Fix_Data_Sync = true,
 
-	NoDeletePlayer = false,
+    NoDeletePlayer = false,
 
-	["Block Pet Gift"] = true,
- 
-	Collect_Cooldown = 60, -- cooldown to collect fruit
-	JustFuckingCollectAll = false, -- Collect all (fruit not wait mutation)
+    ["Block Pet Gift"] = true,
 
-	["Low Cpu"] = true,
-	["Auto Rejoin"] = false,
+    Collect_Cooldown = 60, -- cooldown to collect fruit
 
-	["Rejoin When Update"] = false,
-	["Limit Tree"] = {
-		["Limit"] = 200,
-		["Destroy Untill"] = 200,
+    ["Low Cpu"] = true,
+    ["Auto Rejoin"] = false,
 
-		["Safe Tree"] = {
-			"Tranquil Bloom",
-			"Maple Apple",
-			"Moon Mango",
-			"Bone Blossom",
-			"Fossilight",
-      		"Serenity",
+    ["Rejoin When Update"] = false,
+    ["Limit Tree"] = {
+        ["Limit"] = 250,
+        ["Destroy Untill"] = 230,
 
-		}
-	},
+        ["Safe Tree"] = {
+            "Moon Blossom",
+            "Fossilight",
 
-	Seed = {
-		Buy = {
-			Mode = "Custom", -- Custom , Auto
-			Custom = {
-				"Tomato",
-				"Strawberry",
-				"Bell Pepper",
-				"Blood Banana",
-				"Onion",
-				"Pear",
-				"Grape",
-				"Mushroom",
-				"Pepper",
-				"Cacao",
-				"Beanstalk",
-				"Ember Lily",
-				"Sugar Apple",
-				"Burning Bud",
-				"Giant Pinecone",
-				"Elder Strawberry",
-			}
-		},
-		Place = {
-			Mode = "Lock", -- Select , Lock
-			Select = {
-				"Carrot"
+        }
+    },
+
+    Seed = {
+        Buy = {
+            Mode = "Auto", -- Custom , Auto
+            Custom = {
+                "Carrot",
+            }
+        },
+        Place = {
+            Mode = "Lock", -- Select , Lock
+            Select = {
+                "Carrot"
+            },
+            Lock = {
+                "Maple Apple",
+                "Easter Egg",
+                "Bone Blossom",
+                "Sunflower",
+                "Dragon Pepper",
+                "Elephant Ears",
+                "Moon Melon",
+                "Moon Mango",
+                "Fossilight",
+            }
+        }
+    },
+
+    ["Seed Pack"] = {
+        Locked = {
+
+        }
+    },
+
+    Events = {
+		["Zen Event"] = {
+			["Restocking"] = { -- Minimumthing to restock
+				Max_Restocks_Price = 10000000000,
+				Minimum_Money = 10000000,
+				Minimum_Chi = 30
 			},
-			Lock = {
-				"Sunflower",
-				"Dragon Pepper",
-				"Elephant Ears",
-				"Moon Melon",
-				"Moon Mango",
-				"Fossilight",
+			["Doing"] = {
+				Minimum_Money = 1000, -- minimum money to start play this event
+				First_Upgrade_Tree = 1,
+				Maximum_Chi = 50,
 			}
-		}
-	},
-
-	["Seed Pack"] = {
-		Locked = {
-
-		}
-	},
-
-	Events = {
-		["Cook Event"] = {
-			Minimum_Money = 30_000_000, -- minimum money to start play this event
-			Rewards_Item = { "Sunny-Side Chicken", "Culinarian Chest", "Gorilla Chef", "Gourmet Egg", "Pet Shard Aromatic", "Cooking Cauldron", "Gourmet Seed Pack", "Bitter Melon Seed", "Pricklefruit Seed", "Butternut Squash Seed", "Spring Onion Seed", "Kitchen Crate", "Kitchen Flooring", "Kitchen Cart", "Smoothie Fountain" }
 		},
-		["Traveling Shop"] = {
-			"Bee Egg",
-		},
-		Craft = {
-			"Anti Bee Egg",
-		},
-		Shop = {
-			"Zen Egg",
-			"Koi",
-		},
-		Start_Do_Honey = 2_000_000 -- start trade fruit for honey at money
-	},
+        ["Cook Event"] = {
+            Minimum_Money = 10000000,          -- minimum money to start play this event
+            Rewards_Item = {   -- The top is the most top mean prefered.
+                "Culinarian Chest",
+                "Gourmet Egg",
+                "Gourmet Seed Pack",
+                "Sunny-Side Chicken",
+				"Cooking Cauldron",
+				"Gorilla Chef",
+                -- u can add it more as u want, if it not in list.
+            }
+        },
+        ["Traveling Shop"] = {
+            "Bee Egg",
+        },
+        Craft = {
+            "Ancient Seed Pack",
+            "Anti Bee Egg",
+            "Primal Egg",
+        },
+        Shop = {
+            "Zen Egg",
+            "Zen Seed Pack",
+            "Spiked Mango",
+            "Pet Shard Tranquil",
+            "Pet Shard Corrupted",
+            "Koi",
+            "Soft Sunshine",
+            "Sakura Bush",
+            "Raiju",
+        },
+        Start_Do_Honey = 2000000, -- start trade fruit for honey at money
+    },
 
 	Gear = {
 		Buy = { 
@@ -154,8 +166,10 @@ getgenv().ConfigsKaitun = {
             ["Limit Upgrade"] = 5,-- max is 5 (more than or lower than 1 will do nothing)
             ["Equip When Done"] = {
                 ["Gorilla Chef"] = { 4, 100, 1 },
-				["Sunny-Side Chicken"] = { 3, 100, 2},
-                ["Capybara"] = { 1, 100, 3},
+				["Sushi Bear"] = { 1, 100, 2 },
+				["Sunny-Side Chicken"] = { 3, 100, 3 },
+                ["Capybara"] = { 1, 100, 4 },
+                ["Starfish"] = { 8, 100, 5 },
             },
         },
         Favorite_LockedPet = true,
