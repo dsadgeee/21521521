6587775a10b09cbe5c4f76c125b4314cd19172a0
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-08-13 17:19:34 +0700

    Update AYA.lua

--- AYA.lua
+++ AYA.lua
@@ -153,10 +153,9 @@ getgenv().ConfigsKaitun = {
             },
             ["Limit Upgrade"] = 5,-- max is 5 (more than or lower than 1 will do nothing)
             ["Equip When Done"] = {
-                ["Gorilla Chef"] = { 8, 100, 1 },
-				["Sunny-Side Chicken"] = { 2, 100, 2},
+                ["Gorilla Chef"] = { 4, 100, 1 },
+				["Sunny-Side Chicken"] = { 3, 100, 2},
                 ["Capybara"] = { 1, 100, 3},
-                ["Starfish"] = { 1, 100, 4},
             },
         },
         Favorite_LockedPet = true,
