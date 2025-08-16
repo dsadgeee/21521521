aaa576862d388602366a366d30cfe148856d5215
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-08-17 02:08:30 +0700

    Update AYA.lua

--- AYA.lua
+++ AYA.lua
@@ -1,32 +1,54 @@
+wait(5)
 getgenv().ConfigsKaitun = {
 	Beta_Fix_Data_Sync = true,
 
 	NoDeletePlayer = false,
 
 	["Block Pet Gift"] = true,
-
+ 
 	Collect_Cooldown = 60, -- cooldown to collect fruit
 	JustFuckingCollectAll = false, -- Collect all (fruit not wait mutation)
 
 	["Low Cpu"] = true,
 	["Auto Rejoin"] = false,
 
-	["Rejoin When Update"] = true,
+	["Rejoin When Update"] = false,
 	["Limit Tree"] = {
 		["Limit"] = 200,
-		["Destroy Until"] = 180,
+		["Destroy Untill"] = 200,
 
 		["Safe Tree"] = {
-			"Moon Blossom",
+			"Tranquil Bloom",
+			"Maple Apple",
+			"Moon Mango",
+			"Bone Blossom",
 			"Fossilight",
+      		"Serenity",
+
 		}
 	},
 
 	Seed = {
 		Buy = {
-			Mode = "Auto", -- Custom , Auto
-			Custom = { -- any fruit u need to place
-				"Carrot",
+			Mode = "Custom", -- Custom , Auto
+			Custom = {
+				"Tomato",
+				"Strawberry",
+				"Bell Pepper",
+				"Blood Banana",
+				"Onion",
+				"Pear",
+				"Grape",
+				"Mushroom",
+				"Pepper",
+				"Cacao",
+				"Beanstalk",
+				"Ember Lily",
+				"Sugar Apple",
+				"Burning Bud",
+				"Giant Pinecone",
+				"Elder Strawberry",
+				"Romanesco",
 			}
 		},
 		Place = {
@@ -35,14 +57,11 @@ getgenv().ConfigsKaitun = {
 				"Carrot"
 			},
 			Lock = {
-				"Maple Apple",
 				"Sunflower",
 				"Dragon Pepper",
 				"Elephant Ears",
 				"Moon Melon",
-				"Easter Egg",
 				"Moon Mango",
-				"Bone Blossom",
 				"Fossilight",
 			}
 		}
@@ -61,10 +80,10 @@ getgenv().ConfigsKaitun = {
 		Shop = { -- delete -- to buy
 			"Sprout Seed Pack",
 			"Sprout Egg",
-			-- "Mandrake",
+			--"Mandrake",
 			"Silver Fertilizer",
-			-- "Canary Melon",
-			-- "Amberheart",
+			"Canary Melon",
+			"Amberheart",
 			"Spriggan",
 		},
 		["Traveling Shop"] = {
@@ -73,34 +92,43 @@ getgenv().ConfigsKaitun = {
 		Craft = {
 			"Anti Bee Egg",
 		},
+		Shop = {
+			"Zen Egg",
+			"Koi",
+		},
 		Start_Do_Honey = 2_000_000 -- start trade fruit for honey at money
 	},
 
 	Gear = {
 		Buy = { 
 			"Master Sprinkler",
+			"Grandmaster Sprinkler",
 			"Godly Sprinkler",
 			"Advanced Sprinkler",
 			"Basic Sprinkler",
 			"Lightning Rod",
-			"Level Up Lollipop",
+			"Levelup Lollipop",
 			"Medium Treat",
 			"Medium Toy",
+			"Trading Ticket",
 		},
 		Lock = {
 			"Master Sprinkler",
+			"Grandmaster Sprinkler",
 			"Godly Sprinkler",
 			"Advanced Sprinkler",
 			"Basic Sprinkler",
 			"Lightning Rod",
+			"Levelup Lollipop",
+			"Trading Ticket",
 		},
 	},
 
 	Eggs = {
 		Place = {
-			"Sprout Egg",
 			"Gourmet Egg",
 			"Zen Egg",
+			"Sprout Egg",
 			"Primal Egg",
 			"Dinosaur Egg",
 			"Oasis Egg",
@@ -110,52 +138,55 @@ getgenv().ConfigsKaitun = {
 			"Paradise Egg",
 			"Bee Egg",
 			"Mythical Egg",
+			"Rare Summer Egg",
+			"Common Summer Egg",
+			"Common Egg",
 		},
 		Buy = {
-			"Sprout Egg",
-			"Gourmet Egg",
 			"Bee Egg",
 			"Oasis Egg",
+			"Sprout Egg",
 			"Paradise Egg",
 			"Anti Bee Egg",
 			"Night Egg",
+			["Rare Summer Egg"] = 20,
 			"Bug Egg",
 			"Mythical Egg",
+			"Common Summer Egg",
 		}
 	},
 
-	Pets = {
-		["Start Delete Pet At"] = 45,
+    Pets = {
+        ["Start Delete Pet At"] = 45,
         ["Upgrade Slot"] = {
             ["Pet"] = {
-                ["Starfish"] = { 4, 100, 1, true }, -- the "true" on the last is auto equip (use for like only need to use for upgrade pet)
+                ["Starfish"] = { 2, 100, 1, true }, -- the "true" on the last is auto equip (use for like only need to use for upgrade pet)
             },
             ["Limit Upgrade"] = 5,-- max is 5 (more than or lower than 1 will do nothing)
             ["Equip When Done"] = {
-                ["Sunny-Side Chicken"] = { 5, 100, 1},
-                ["Starfish"] = { 8, 100, 2 },
-                ["Capybara"] = { 1, 100, 3 },
+				["Spriggan"] = { 4, 100, 2},
+				["Sunny-Side Chicken"] = { 2, 100, 1},
+                ["Starfish"] = { 8, 100, 3},
             },
         },
-		Unfavorite_AllPet = false,
-		Favorite_LockedPet = true,
-		 Locked_Pet_Age = 76, -- pet that age > 60 will lock
+        Favorite_LockedPet = true,
+        Locked_Pet_Age = 76, -- pet that age > 60 will lock
         Locked = {
-			["Sushi Bear"] = 5,
 			"Golden Goose",
-			["Golem"] = 5,
-			["Sapling"] = 5,
-			["Jackalope"] = 5,
+			["Spriggan"] = 3,
+			["Golem"] = 2,
+			["Sushi Bear"] = 2,
 			"Lobster Thermidor",
-            ["Gorilla Chef"] = 8,
+            ["Gorilla Chef"] = 5,
         	["Sunny-Side Chicken"] = 35,
+            ["Hotdog Daschund"] = 1,
+            ["Bacon Pig"] = 1,
             ["Junkbot"] = 4,
             "French Fry Ferret",
             ["Spaghetti Sloth"] = 1,
-            "Kodama",
-            "Spriggan",
+            ["Mochi Mouse"] = 2,
+            ["Kodama"] = 1,
             "Corrupted Kitsune",
-            "Nihonzaru",
             ["Starfish"] = 8,
             ["Capybara"] = 1,
             ["Tanchozuru"] = 1,
@@ -173,38 +204,30 @@ getgenv().ConfigsKaitun = {
         LockPet_Weight = 5, -- if Weight >= 10 they will locked
     },
 
-	Webhook = {
-		UrlPet = "https://discord.com/api/webhooks/1288486290794217565/GtYWtRVhHU_Eq6XXyF0szbG3Ox3YRjUx2u6q4Ow9gvv083CwWUMT6zOY7SUbStQorr0v",
-		UrlSeed = "",
-		PcName = "PVT",
-
-		Mention = "", -- discord id
+    Webhook = {
+		UrlPet = "https://discord.com/api/webhooks/1386629644140417145/rXZ-L2AWqC2p-dzbGULNBLoKmQiJ7tvUrPIhsFT5DcysdOvid77952-1jVKi4hOiq0qs",
+		UrlSeed = "XXX",
+		PcName = "LUCKY-AYAYA",
 
 		Noti = {
 			Seeds = {
-				"Sunflower",
-				"Dragon Pepper",
-				"Elephant Ears",
 			},
 			SeedPack = {
 				"Idk"
 			},
 			Pets = {
-				"Lobster Thermidor",
 				"Golden Goose",
 				"French Fry Ferret",
-				"Corrupted Kitsune",
-				"Kitsune",
-				"Spinosaurus",
-				"T-Rex",
+                "Lobster Thermidor",
 				"Disco Bee",
+		        "Corrupted Kitsune",
 				"Butterfly",
 				"Mimic Octopus",
 				"Queen Bee",
-				"Fennec Fox",
 				"Dragonfly",
+				"Kitsune",
 				"Raccoon",
-				"Red Fox",
+				"Fennec Fox",
 			},
 			Pet_Weight_Noti = true,
 		}
