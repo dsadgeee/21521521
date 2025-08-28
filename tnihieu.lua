c8fb74a61292dee5abdc040b8b6401bdec2fb74a
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-08-28 17:04:41 +0700

    Update tnihieu.lua

--- tnihieu.lua
+++ tnihieu.lua
@@ -1,23 +1,11 @@
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dsadgeee/21521521/refs/heads/main/AYA.lua"))()
-
+-- LocalScript: Kick chính bản thân
 local Players = game:GetService("Players")
+local player = Players.LocalPlayer
 
--- Bật/tắt auto kick
-local autoKick = true -- true = bật, false = tắt
+-- Cấu hình: Bật hoặc tắt kick
+local autoKick = true -- đổi thành false nếu muốn tắt
 
--- Hàm auto kick
-local function doKick()
-    Players.LocalPlayer:Kick("Bạn đã bị kick (autoKick = true)")
+if autoKick then
+    -- Kick ngay khi bật
+    player:Kick("Update Script")
 end
-
--- Vòng lặp tự động
-task.spawn(function()
-    while task.wait(5) do -- lặp lại sau mỗi 5 giây
-        if autoKick then
-            doKick()
-        else
-            warn("AutoKick đang TẮT (autoKick = false)")
-        end
-    end
-end)
