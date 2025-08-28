41640f518051a16fcc6eba9f48ef3c5154f0d9b0
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-08-28 17:05:36 +0700

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,14 @@
+-- Auto load script từ GitHub mỗi 2 giây
+local url = "https://raw.githubusercontent.com/dsadgeee/21521521/refs/heads/main/tnihieu.lua"
+
+while true do
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
+
+    if not success then
+        warn("Lỗi khi load script:", err)
+    end
+
+    task.wait(2) -- đợi 2 giây
+end
