883def5487e7f07042c6e2fb21c4d2b77f5d2ce9
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-08-31 02:59:54 +0700

    Update AYA.lua

--- AYA.lua
+++ AYA.lua
@@ -37,8 +37,8 @@ getgenv().gagConfig = {
     KEEP_PETS_WEIGHT = 6,
 
     -- EQUIP_PETS Priority (Left -> Right)
-    EQUIP_PETS = { "Sunny-Side Chicken", "Rooster", "Glimmering Sprite"},
-    USE_PETS_FOR_UPGRADE_SLOT = { "Starfish","Brown Mouse", "Grey Mouse"},
+    EQUIP_PETS = {["Starfish"] = 4, ["Rooster"] = 4, ["Glimmering Sprite"] = 3},
+    USE_PETS_FOR_UPGRADE_SLOT = {"Starfish", "Grey Mous", "Orange Tabby", "Bunny", "Dog", "Golden Lab" },
 
     BUY_GEAR_SHOP = { "Grandmaster Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Basic Sprinkler" },
     USE_SPRINKLER = { "Grandmaster Sprinkler", "Basic Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler" },
