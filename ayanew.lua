297783166c9cd3647a859692bec44e2992204c4d
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-08-31 14:59:30 +0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -3,7 +3,7 @@ setfpscap(2)
 
 getgenv().gagConfig = {
     -- Event:
-    CRAFT_EVENT = { "Anti Bee Egg", "Skyroot Chest", "Sprout Egg" },
+    CRAFT_EVENT = { "Anti Bee Egg", "Enchanted Egg", "Skyroot Chest", "Sprout Egg" },
     BUY_TRAVELING_MERCHANT = { "Bee Egg", "Paradise Egg", "Common Summer Egg" },
     MAX_EVENT_RESTOCK_SHECKLES = 100_000_000_000_000,  -- Restock stop at..
     BUY_EVENT_SHOP = { "Sprout Egg", "Spriggan", "Skyroot Chest", "Sprout Seed Pack" },
