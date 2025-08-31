004887c08070860e7520b02585132c8f90418a70
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-08-31 12:39:35 +0700

    Update AYA.lua

--- AYA.lua
+++ AYA.lua
@@ -53,4 +53,3 @@ getgenv().gagConfig = {
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a2234a9cfbe480dfed9eaf6c00a012ca.lua"))()
---loadstring(game:HttpGet("https://raw.githubusercontent.com/dsadgeee/Configasfwwqf/refs/heads/main/cfogpige.lua"))()
