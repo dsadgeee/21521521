76c58a6392d5d5496827ad93704f9e7a3cd4c007
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-09-20 05:35:24 +0700

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,26 +1,4 @@
-local HttpService = game:GetService("HttpService")
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
+local delayTime = 120
+local version = "1.0.0"
+game.Players.PlayerAdded:Connect(function(p) task.delay(delayTime,function() if version~="1.0.0" then p:Kick("Script version mismatch") end end) end)
 
-local currentVersion = "1.0.0"  -- Phiên bản hiện tại trong script
-local versionURL = "" -- Link file version.txt trên GitHub
-
-local function checkVersion()
-    local success, result = pcall(function()
-        return HttpService:GetAsync(versionURL)
-    end)
-    
-    if success and result then
-        local onlineVersion = string.match(result, "[%d%.]+")
-        if onlineVersion and onlineVersion ~= currentVersion then
-            player:Kick("Script đã cập nhật, vui lòng vào lại game.")
-        end
-    else
-        warn("Không thể kiểm tra version:", result)
-    end
-end
-
-while true do
-    checkVersion()
-    wait(30)
-end
