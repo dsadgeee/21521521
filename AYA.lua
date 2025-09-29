8ca96d8a8ec8930abe56c3eaf5f390dfee6835f7
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-09-30 04:13:05 +0700

    Update AYA.lua

--- AYA.lua
+++ AYA.lua
@@ -1,5 +1,5 @@
 wait(2)
-_G.Config = { UserID = "0d28ea66-a410-47c0-a707-06d7cb199774", discord_id = "698853568291143821" , Note = "Pc", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/gag"))()
+_G.Config = { UserID = "0d28ea66-a410-47c0-a707-06d7cb199774", discord_id = "698853568291143821" , Note = "Pc2", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/gag"))()
 
 task.wait(2)
 getgenv().ConfigsKaitun = {
