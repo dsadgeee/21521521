867704435aa7181bf6b612958ea780843b1cd4b0
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-09-30 04:21:46 +0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -276,6 +276,6 @@ getgenv().ConfigsKaitun = {
 License = "4vWx20QgVjGB2bC68YTGTKTo4OyjKSaP"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Real-Aya/Loader/main/Init.lua'))()
 
-task.wait(3)
+wait(3)
 _G.Config = { UserID = "0d28ea66-a410-47c0-a707-06d7cb199774", discord_id = "698853568291143821" , Note = "Pc1", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/gag"))()
 
