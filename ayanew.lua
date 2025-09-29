42bcc6d884687506f99f4bf691f7956aefff3aab
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-09-30 04:16:56 +0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -1,4 +1,3 @@
-wait(2)
 _G.Config = { UserID = "0d28ea66-a410-47c0-a707-06d7cb199774", discord_id = "698853568291143821" , Note = "Pc1", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/gag"))()
 
 task.wait(2)
