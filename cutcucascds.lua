acfff4cab288cf1b24a611ce2d3ffb2e08e131ce
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-06 10:44:01 -0700

    Update and rename a to cutcucascds.lua

---
+++ cutcucascds.lua
@@ -0,0 +1,35 @@
+script_key="CnkuObJwgnkKiglxzVEsyiafKNFOtMtN";
+
+getgenv().pvbConfig = {
+    AUTO_UPDATE_RESTART = true,
+    MAX_FPS = 3,  -- This will override setfpscap()
+    LOW_CPU = true,
+    MAX_REBIRTH = 99,  -- Stop rebirth at set amount
+    FORCE_REBIRTH_IGNORE_KEEP_BRAINROT = true,  -- Ignore KEEP_BRAINROT related config until max rebirth
+    FROST_GRENADE_TARGET_MAX_HP = 100000,  -- Use frost grenade 100k+ hp brainrot
+    
+    OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
+    FUSE_PLANT = {"Mr Carrot" ,},  -- Auto keep & fuse required plant + brainrot
+
+    BUY_SEED_SHOP = {["Cactus"] = 5, ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, "Cocotank", "Carnivorous Plant", "Mr Carrot", "Tomatrio", "Shroombino", "Mango"},
+    BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
+    KEEP_SEED = {},
+    KEEP_PLANT_RARITY = {"Secret", "Limited"},
+    KEEP_BRAINROT_MONEY_PER_SECOND = 50000,  -- Number
+    KEEP_BRAINROT_RARITY = {},
+
+    SELL_BRAINROT_DELAY = 30,
+    SELL_PLANT_DELAY = 30,
+
+    -- Webhook
+    BRAINROT_WEBHOOK_URL = "https://discord.com/api/webhooks/1283496015286112306/6GynCAV9qP7lv4kCSkAZ7RY37TbpqkZROFwoHmvVtIgq4QYLywx9oDyg1rEy-6fkkAKI",
+    DISCORD_ID = "698853568291143821",
+    NOTIFY_RARITY = {},
+    NOTIFY_MONEY_PER_SECOND = 10000,
+    WEBHOOK_NOTE = "",
+    SHOW_PUBLIC_DISCORD_ID = true,
+    SHOW_WEBHOOK_USERNAME = true,
+    SHOW_WEBHOOK_JOBID = true,
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1955a9eeb0a6b663051651121e75f7f7.lua"))()
