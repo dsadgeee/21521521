c45166b4295a00937e09f50e2ede38894a67a2ca
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-07 13:05:41 -0700

    Update savemamut.lua

--- savemamut.lua
+++ savemamut.lua
@@ -1,179 +1,51 @@
-
+script_key="vldTBYdTWcXhTNTLAihuPPUqSKdjaxgE";
 setfpscap(2)
 
 getgenv().gagConfig = {
     -- Event:
-    CRAFT_EVENT = { "Anti Bee Egg", "Skyroot Chest" , "Sprout Egg" },
-    BUY_TRAVELING_MERCHANT = { "Bee Egg", "Paradise Egg", "Rare Summer Egg", "Common Summer Egg", "Pitcher Plant", "Feijoa", "Loquat" },
-    BUY_EVENT_SHOP = { "Fall Egg" },
-    
-    PLANT_EVENT_TREES = true,  -- This config will replace most plant related config
-    PLACE_COSMETIC = { "Cooking Kit" },
+    BUY_TRAVELING_MERCHANT = { "Bee Egg", "Loquat", "Feijoa", "Pitcher Plant" },
     
     -- General:
-    AUTO_UPDATE_RESTART = false,
+    AUTO_UPDATE_RESTART = true,
     REDEEM_CODES = {},
     EXTRA_PET_SLOTS = 5,
     EXTRA_EGG_SLOTS = 5,
-    MAX_REBIRTH_SHECKLES = 1_000_000_000_000,
     ADD_FRIEND = true,
-    OPEN_ALL_SEED_PACK = false,
-
-    BUY_COSMETIC_SHOP = { ["Cooking Kit"] = 3 },
+    OPEN_ALL_SEED_PACK = true,
 
     MAX_PLANTS = 250,
     DESTROY_UNTIL_MIN_PLANTS = 200,
     DELETE_PLANTS_AFTER_MAX = { "Carrot", "Strawberry", "Blueberry", "Tomato", "Apple" },
     
     BUY_EGGS = { "Bug Egg", "Bee Egg", "Paradise Egg", "Mythical Egg", "Rare Summer Egg", "Common Summer Egg", "Rare Egg", "Uncommon Egg" },
-    PLANT_EGGS = { "Fall Egg" , "Anti Bee Egg" , "Common Summer Egg", "Rare Egg" },
+    PLANT_EGGS = { "Enchanted Egg", "Sprout Egg", "Gourmet Egg", "Corrupted Zen Egg", "Zen Egg", "Dinosaur Egg", "Primal Egg", "Anti Bee Egg", "Bee Egg", "Night Egg", "Bug Egg", "Paradise Egg", "Mythical Egg", "Common Summer Egg", "Rare Egg", "Uncommon Egg" },
     
-    BUY_SEED_SHOP = { "Romanesco", "Elder Strawberry", "Giant Pinecone", "Burning Bud", "Sugar Apple", "Ember Lily", "Beanstalk", "Cacao", "Pepper", "Mushroom", "Grape", "Mango", "Dragon Fruit", "Cactus", ["Coconut"] = 50, ["Bamboo"] = 50, ["Apple"] = 50, ["Pumpkin"] = 50, ["Watermelon"] = 50, ["Daffodil"] = 50, ["Tomato"] = 50, ["Orange Tulip"] = 50, ["Blueberry"] = 50, ["Strawberry"] = 50, ["Carrot"] = 50 },
+    BUY_SEED_SHOP = { "Crimson Thorn", "Romanesco", "Elder Strawberry", "Giant Pinecone", "Burning Bud", "Sugar Apple", "Ember Lily", "Beanstalk", "Cacao", "Pepper", "Mushroom", "Grape", "Mango", "Dragon Fruit", "Cactus", ["Coconut"] = 50, ["Bamboo"] = 50, ["Apple"] = 50, ["Pumpkin"] = 50, ["Watermelon"] = 50, ["Daffodil"] = 50, ["Tomato"] = 50, ["Orange Tulip"] = 50, ["Blueberry"] = 50, ["Strawberry"] = 50, ["Carrot"] = 50 },
     KEEP_SEEDS = { "Bone Blossom" },
     KEEP_SEEDS_AFTER_MAX_PLANTS = { "Carrot", "Strawberry", "Blueberry", "Tomato", "Apple" },
     
     FAVOURITE_FRUIT_MUTATIONS = {},
     SKIP_HARVEST_MUTATIONS = {},
 
-    KEEP_PETS = { ["Barn Owl"] = 8, "Swan", ["Space Squirrel"] = 3, "Cockatrice", "Drake", "Wisp", "Luminous Sprite", "Wisp Well", "Phoenix", ["Glimmering Sprite"] = 8, "Griffin", ["Brown Mouse"] = 2, ["Squirrel"] = 1, ["Grey Mouse"] = 2, ["Rooster"] = 4, "Lemon Lion", "Peach Wasp", "Chicken Zombie", ["Gorilla Chef"] = 8, "Green Bean", "Golden Goose", ["Spriggan"] = 1, "Lobster Thermidor", ["Sunny-Side Chicken"] = 35, "French Fry Ferret", ["Kodama"] = 1, "Corrupted Kitsune", ["Starfish"] = 10, ["Capybara"] = 1, ["Seal"] = 1, "Kitsune", "Blood Kiwi", "Disco Bee", "Raccoon", "Queen Bee", "Dragonfly", "Butterfly", "Mimic Octopus", "Red Fox", "Bee", "Honey Bee", "Bear Bee", "Petal Bee", "Wasp", "Tarantula Hawk" },
+    KEEP_PETS = { "Idol Chipmunk", "Chinchilla", "Mizuchi", "Tiger", "Red Panda", "Barn Owl", "Swan", "Space Squirrel", "Cockatrice", "Drake", "Wisp", "Luminous Sprite", "Wisp Well", "Phoenix", "Griffin", "Brown Mouse", "Squirrel", "Grey Mouse", "Rooster", "Lemon Lion", "Peach Wasp", "Chicken Zombie", "Gorilla Chef", "Green Bean", "Golden Goose", "Spriggan", "Lobster Thermidor", "Sunny-Side Chicken", "French Fry Ferret", "Kodama", "Corrupted Kitsune", "Starfish", "Capybara", "Seal", "Kitsune", "Blood Kiwi", "Disco Bee", "Raccoon", "Queen Bee", "Dragonfly", "Butterfly", "Mimic Octopus", "Red Fox", "Bee", "Honey Bee", "Bear Bee", "Petal Bee", "Wasp", "Tarantula Hawk" }
     KEEP_PETS_WEIGHT = 5,
+    KEEP_PETS_AGE = 90,c
 
-    EQUIP_PETS = {["Wasp"] = 1, ["Tarantula Hawk"] = 1, ["Barn Owl"] = 1, ["Sunny-Side Chicken"] = 5, ["Starfish"] = 5, ["Brown Mouse"] = 3, ["Grey Mouse"] = 3, ["Rooster"] = 4, "Bear Bee", "Petal Bee"},
-    USE_PETS_FOR_UPGRADE_SLOT = { ["Barn Owl"] = 3, ["Brown Mouse"] = 3, ["Grey Mouse"] = 3, ["Starfish"] = 5 },
-    REMOVE_PET_MAX_UPGRADE = { "Chicken" },  -- Unequip from garden
+    EQUIP_PETS = { ["Rooster"] = 2, ["Capybara"] = 1, ["Chicken"] = 5 },
+    USE_PETS_FOR_UPGRADE_SLOT = { "Chicken" },
+    REMOVE_PET_MAX_UPGRADE = { "Capybara", "Chicken" },  -- Unequip from garden
 
     BUY_GEAR_SHOP = { "Grandmaster Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Basic Sprinkler", "Trading Ticket" },
     USE_SPRINKLER = { "Basic Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Grandmaster Sprinkler" },
 
-    PET_WEBHOOK_URL = "https://discord.com/api/webhooks/1408866663558025278/ohhAUmTACbJP2YbxoGH8iXRYJpT36w0HcHrffSsza8TA6iw62VUO1Sb2jCBavqeK1FeW", 
-    NOTIFY_PETS = { "Swan", "Cockatrice", "Gnome", "Griffin", "Golden Goose", "Corrupted Kitsune", "Raiju", "Mizuchi", "Kitsune", "Fennec Fox", "Disco Bee", "Raccoon", "Queen Bee", "Night Owl", "Dragonfly", "Butterfly", "Mimic Octopus", "Red Fox" },
+    PET_WEBHOOK_URL = "https://discord.com/api/webhooks/1408866660017766500/8OgtYYAinAJ9qhpqLxr_jIzUztGg0YqnxzayJql_zAyaYqzoXxUH2OhEoo9DX09HpDOF", 
+    SHOW_PUBLIC_DISCORD_ID = true,
+    NOTIFY_PETS = {},
     NOTIFY_PETS_WEIGHT = 5,
-    DISCORD_ID = "940582401464217640",
-    WEBHOOK_NOTE = "pc1",
-    SHOW_WEBHOOK_USERNAME = false,
-    SHOW_WEBHOOK_JOBID = false,
+    DISCORD_ID = "698853568291143821",
+    WEBHOOK_NOTE = "Pc",
+    SHOW_WEBHOOK_USERNAME = true,
+    SHOW_WEBHOOK_JOBID = true,
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6cb882027ef5de19462b160764dcfb53.lua"))()
-wait(4)
--- Script generated by SimpleSpy - credits to exx#9394
-local Rep = game:GetService("ReplicatedStorage")
-
-task.spawn(function()
-    while task.wait(1) do -- delay 1 giây để tránh kick
-        -- 🛒 Mua Fall Egg
-        local args1 = {
-            [1] = "Fall Egg",
-            [2] = 3
-        }
-        Rep.GameEvents.BuyEventShopStock:FireServer(unpack(args1))
-        print("🛒 Đã mua:", args1[1])
-
-        -- 🛒 Mua Space Squirrel
-        local args2 = {
-            [1] = "Space Squirrel",
-            [2] = 3
-        }
-        Rep.GameEvents.BuyEventShopStock:FireServer(unpack(args2))
-        print("🛒 Đã mua:", args2[1])
-    end
-end)
-
-local Players = game:GetService('Players')
-local Rep = game:GetService('ReplicatedStorage')
-local CollectionService = game:GetService('CollectionService')
-
-local localPlayer = Players.LocalPlayer
-local CollectRemote = Rep.GameEvents.Crops.Collect
-local SubmitAllRemote = Rep.GameEvents.FallMarketEvent.SubmitAllPlants
-
--- ⚙️ Delay và quét
-local USE_FARM_ONLY = true
-local FIRE_DELAY = 1
-local INTERVAL = 10
-
--- 📂 Farm folder
-local farmFolder
-pcall(function()
-    if
-        workspace:FindFirstChild('Farm')
-        and workspace.Farm:FindFirstChild('Farm')
-    then
-        farmFolder = workspace.Farm.Farm
-    end
-end)
-
--- 🏷️ Các loại cây cần thu hoạch
-local targetCrops = {
-    ['Mushroom'] = true,
-    ['Glowthorn'] = true,
-    ['Pepper'] = true,
-    ['Cacao'] = true,
-    ['Apple'] = true,
-    ['Wispwing'] = true,
-    ['Romanesco'] = true,
-    ['Elder Strawberry'] = true,
-    ['Burning Bud'] = true,
-    ['Giant Pinecone'] = true,
-    ['Corn'] = true,
-    ['Sugar Apple'] = true,
-    ['Ember Lily'] = true,
-    ['Dragon Fruit'] = true,
-    ['Sunbulb'] = true,
-    ['Orange Tulip'] = true,
-    ['Mango'] = true,
-    ['Cactus'] = true,
-    ['Beanstalk'] = true,
-    ['Lightshoot'] = true,
-    ['Grape'] = true,
-    ['Daffodil'] = true,
-    ['Aurora Vine'] = true,
-    ['Grand Tomato'] = true,
-    ['Maple Apple'] = true,
-    ['Princess Thorn'] = true,
-    ['Spiked Mango'] = true,
-    ['Pineapple'] = true,
-    ['King Cabbage'] = true,
-    ['Carnival Pumpkin'] = true,
-    ['Kniphofia'] = true,
-    ['Golden Peach'] = true,
-    ['Maple Resin'] = true,
-}
-
--- 🏷️ Kiểm tra tag thu hoạch
-local function hasCollectTag(obj)
-    if type(obj.HasTag) == 'function' then
-        local ok, res = pcall(function()
-            return obj:HasTag('CollectPrompt')
-        end)
-        if ok then
-            return res
-        end
-    end
-    return CollectionService:HasTag(obj, 'CollectPrompt')
-end
-
--- 🌱 Thu hoạch và nộp ngay sau mỗi lần
-local function harvestAndSubmit()
-    local descendants = USE_FARM_ONLY and farmFolder:GetDescendants()
-        or workspace:GetDescendants()
-
-    for _, inst in ipairs(descendants) do
-        if inst:IsA('ProximityPrompt') and hasCollectTag(inst) then
-            local crop = inst.Parent and inst.Parent.Parent
-            if crop and targetCrops[crop.Name] then
-                -- Thu hoạch
-                CollectRemote:FireServer({ crop })
-                task.wait(FIRE_DELAY)
-
-                -- Nộp ngay sau khi thu hoạch cây này
-                SubmitAllRemote:FireServer()
-            end
-        end
-    end
-end
-
--- 🔄 Loop
-while task.wait(INTERVAL) do
-    harvestAndSubmit()
-end
