42841910a64e981816d354b2255cc4498bced183
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-23 13:47:55 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -1,65 +1,22 @@
-repeat task.wait() until game:IsLoaded()
-local HttpService = game:GetService("HttpService")
-local Players = game:GetService("Players")
-script_key="GRBNBkkgWdoICBmTzzeMewauyGQuCWXz";
-setfpscap(2)
-
-getgenv().gagConfig = {
-    -- Event:
-    CRAFT_EVENT = { "Anti Bee Egg" },
-    BUY_TRAVELING_MERCHANT = { "Bee Egg", "Paradise Egg", "Fall Egg", "Loquat", "Feijoa", "Pitcher Plant" },
-    BUY_EVENT_SHOP = { "Spooky Egg", ["Ghost Bear"] = 4, ["Reaper"] = 3 },
-    
-    MAX_PLANTS = 200,
-    DESTROY_UNTIL_MIN_PLANTS = 400,
-    KEEP_SEEDS = { "Bone Blossom" },
-    KEEP_SEEDS_AFTER_MAX_PLANTS = {},
-
-    PLACE_COSMETIC = { "Cooking Kit" },
-    
-    -- General:
-    AUTO_UPDATE_RESTART = true,
-    REDEEM_CODES = {},
-    EXTRA_PET_SLOTS = 5,
-    EXTRA_EGG_SLOTS = 5,
-    MAX_REBIRTH_SHECKLES = 0,
-    ADD_FRIEND = true,
-    OPEN_ALL_SEED_PACK = true,
-    COLLECT_FRUIT_PER_SECOND = 3,
-
-    BUY_COSMETIC_SHOP = { ["Cooking Kit"] = 3 },
-    
-    BUY_EGGS = { "Spooky Egg", "Jungle Egg", "Bug Egg", "Bee Egg", "Paradise Egg", "Mythical Egg", "Rare Summer Egg", "Common Summer Egg", "Rare Egg", "Uncommon Egg" },
-    PLANT_EGGS = { "Spooky Egg", "Jungle Egg", "Anti Bee Egg", "Fall Egg"},
-    
-    BUY_SEED_SHOP = { "Great Pumpkin", "Crimson Thorn", "Romanesco", "Elder Strawberry", "Giant Pinecone", "Burning Bud", "Sugar Apple", "Ember Lily", "Beanstalk", "Cacao", "Pepper", "Mushroom", "Grape", "Mango", "Dragon Fruit", "Cactus", ["Coconut"] = 50, ["Bamboo"] = 50, ["Apple"] = 50, ["Pumpkin"] = 50, ["Watermelon"] = 50, ["Daffodil"] = 50, ["Corn"] = 50, ["Tomato"] = 50, ["Orange Tulip"] = 50, ["Blueberry"] = 50, ["Strawberry"] = 50, ["Carrot"] = 50 },
-    
-    FAVOURITE_FRUIT_MUTATIONS = {},
-    SKIP_HARVEST_MUTATIONS = {},
-
-
-    KEEP_PETS = {"Hex Serpent", ["Ghost Bear"] = 8, "Headless Horseman", ["Reaper"] = 3, ["Black Cat"] = 5, ["Bat"] = 2, ["Idol Chipmunk"] = 4,"Ostrich","Flamingo", "Chinchilla", "Mizuchi", "Tiger", "Red Panda", "Barn Owl", "Swan", ["Space Squirrel"] = 3, "Cockatrice", "Drake", "Wisp", "Luminous Sprite", "Wisp Well", "Phoenix", "Griffin", ["Brown Mouse"] = 1, ["Squirrel"] = 1, ["Grey Mouse"] = 1, ["Rooster"] = 5, "Lemon Lion", "Peach Wasp", "Chicken Zombie", ["Gorilla Chef"] = 8, "Green Bean", "Golden Goose", ["Spriggan"] = 1, "Lobster Thermidor", ["Sunny-Side Chicken"] = 25, "French Fry Ferret", ["Kodama"] = 1, "Corrupted Kitsune", ["Starfish"] = 10, "Capybara", ["Seal"] = 8, "Peacock", "Kitsune", "Blood Kiwi", "Disco Bee", "Raccoon", "Queen Bee", "Dragonfly", "Butterfly", "Mimic Octopus", "Red Fox", "Bee", "Honey Bee", "Bear Bee", "Petal Bee", ["Wasp"] = 5, ["Tarantula Hawk"] = 5},
-    KEEP_PETS_WEIGHT = 7,
-    KEEP_PETS_AGE = 90,
-    
-    EQUIP_PETS = { {"Ghost Bear", 2, 1}, {"Rooster", 5, 2}, {"Wasp", 1, 3}},
-    USE_PETS_FOR_UPGRADE_SLOT = { "Rooster" },
-
-    BUY_GEAR_SHOP = { "Grandmaster Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Basic Sprinkler", "Trading Ticket", "Watering Can", "Levelup Lollipop" },
-    USE_SPRINKLER = { "Basic Sprinkler", "Master Sprinkler", "Godly Sprinkler", "Advanced Sprinkler", "Grandmaster Sprinkler" },
-
-    PET_WEBHOOK_URL = "https://discord.com/api/webhooks/1408866663558025278/ohhAUmTACbJP2YbxoGH8iXRYJpT36w0HcHrffSsza8TA6iw62VUO1Sb2jCBavqeK1FeW",
-    SHOW_PUBLIC_DISCORD_ID = true,
-    NOTIFY_PETS = { "Headless Horseman","Hex Serpent"},
-    NOTIFY_PETS_WEIGHT = 7,
-    DISCORD_ID = "796010306966388777",
-    WEBHOOK_NOTE = "",
-    SHOW_WEBHOOK_USERNAME = false,
-    SHOW_WEBHOOK_JOBID = true,
-
-    KICK_AFTER_GIFT_PET = false,
-    GIFT_USERNAME = {},
-    GIFT_PET = {},
+wait(3)
+getgenv().Settings = {
+    Mailing = {
+        ["Diamonds"] = {
+            Class = "Currency",
+            Amount = "All",        -- gửi toàn bộ khi đạt ngưỡng
+            MinDiamonds = 20000000 -- ngưỡng tối thiểu (ví dụ: 20 triệu)
+        },
+    },
+    Users = {
+        "DreamSoCow",  -- người nhận
+    },
+    ["Split Items Evenly"] = false,
+    ["Only Online Accounts"] = false,
+    ["Developer Mode"] = false,
+    [[ THANK YOU, YOUR MOM SO FAT <3! ]]
 }
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cholacoho123/fuck.lua/refs/heads/main/halloween%20event%20p99.lua"))()
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c916e5b90dc37c71ecf1ec00dfce3d5d.lua"))()
+wait(10)
+_G.Config = { UserID = "0d28ea66-a410-47c0-a707-06d7cb199774", discord_id = "698853568291143821" , Note = "Pc", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/pet99"))()
+end)
