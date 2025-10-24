0762f56bcd1ed962485404af1b10004c751d5f7e
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-24 16:11:02 -0700

    Update pet99.lua

--- pet99.lua
+++ pet99.lua
@@ -17,5 +17,14 @@ getgenv().Settings = {
     ["Developer Mode"] = false,
     [[ THANK YOU, YOUR MOM SO FAT <3! ]]
 }
+
+-- ⚙️ CONFIG
+_G.Config = {
+    UserID = "0d28ea66-a410-47c0-a707-06d7cb199774",
+    discord_id = "698853568291143821",
+    Note = "Pc",
+}
+
 -- 🧩 LOAD SCRIPTS
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dsadgeee/21521521/refs/heads/main/track.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/pet99"))()
