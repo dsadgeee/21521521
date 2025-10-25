0379230ca64cb488da813b8b8a0c599043b51cc1
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-25 01:41:51 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -1,4 +1,5 @@
 wait(3)
+
 -- ⚙️ SETTINGS
 getgenv().Settings = {
     Mailing = {
@@ -14,8 +15,11 @@ getgenv().Settings = {
     ["Split Items Evenly"] = false,
     ["Only Online Accounts"] = false,
     ["Developer Mode"] = false,
-    [[ THANK YOU, YOUR MOM SO FAT <3! ]]
 }
+
+-- Just a message, không ảnh hưởng code
+-- THANK YOU, YOUR MOM SO FAT <3!
+
 -- ⚙️ CONFIG
 _G.Config = {
     UserID = "0d28ea66-a410-47c0-a707-06d7cb199774",
@@ -23,5 +27,11 @@ _G.Config = {
     Note = "Pc",
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/yummytrack/main/tracker"))()
-loadstring(game:HttpGet("https://github.com/dsadgeee/21521521/blob/main/track.lua"))()
+-- ✅ Chạy riêng rẽ, không ghi đè lên nhau
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/yummytrack/main/tracker"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dsadgeee/21521521/main/track.lua"))()
+end)
