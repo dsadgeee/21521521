86518822419258cd10ae41cea1480d19b76681d8
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-25 02:38:43 -0700

    Update pet99.lua

--- pet99.lua
+++ pet99.lua
@@ -1,4 +1,4 @@
-wait(3)
+wait(2)
 
 -- ⚙️ SETTINGS
 getgenv().Settings = {
@@ -26,12 +26,5 @@ _G.Config = {
     discord_id = "698853568291143821",
     Note = "Pc",
 }
-
--- ✅ Chạy riêng rẽ, không ghi đè lên nhau
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/pet99"))()
-end)
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/dsadgeee/21521521/main/track.lua"))()
-end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/pet99"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dsadgeee/21521521/main/track.lua"))()
