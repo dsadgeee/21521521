402bb36f5a0d0fb89d6e5e8107006f9d20af5fe2
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-24 23:47:22 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -1,23 +1,4 @@
 wait(3)
-
--- ⚙️ SETTINGS
-getgenv().Settings = {
-    Mailing = {
-        ["Diamonds"] = {
-            Class = "Currency",
-            Amount = "All",        -- gửi toàn bộ khi đạt ngưỡng
-            MinDiamonds = 20000000 -- ngưỡng tối thiểu (ví dụ: 20 triệu)
-        },
-    },
-    Users = {
-        "DreamSoCow",  -- người nhận
-    },
-    ["Split Items Evenly"] = false,
-    ["Only Online Accounts"] = false,
-    ["Developer Mode"] = false,
-    [[ THANK YOU, YOUR MOM SO FAT <3! ]]
-}
-
 -- ⚙️ CONFIG
 _G.Config = {
     UserID = "0d28ea66-a410-47c0-a707-06d7cb199774",
