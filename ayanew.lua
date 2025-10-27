d31a77c7ad1c0785e18eecf1babf74734596a9c8
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-26 17:40:57 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -1,53 +1,61 @@
-    getgenv().Settings = {
-        Mailing = {
-            ["Diamonds"] = {
-                Class = "Currency",
-                Amount = "All",
-                MinDiamonds = 50000000
-            },
-            ["Huge"] = {
-                Class = "Pet",
-                Rarity = "Huge",
-                Amount = "All"
-            },
-            ["Lootbox"] = {
-                Class = "Pet",
-                Egg = true,
-                Amount = "All"
-            },
-            -- ✅ ADDED: Titanic
-            ["Titanic"] = {
-                Class = "Pet",
-                Rarity = "Titanic",
-                Amount = "All"
-            },
-            -- ✅ ADDED: Exclusive Pet
-            ["Exclusive"] = {
-                Class = "Pet",
-                Rarity = "Exclusive",
-                Amount = "All"
-            },
-        },
+setfpscap(3)
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+UserSettings():GetService("UserGameSettings").MasterVolume = 0
+UserSettings():GetService("UserGameSettings").SavedQualityLevel = 1
+workspace.LevelOfDetail = Enum.ModelLevelOfDetail.Disabled
+game:GetService("Lighting").GlobalShadows = false
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
+settings().Rendering.MeshPartDetailLevel = Enum.MeshPartDetailLevel.Level04
+settings().Rendering.GraphicsMode = 9
 
-        Users = {
-            "DreamSoCow",
-        },
-
-        ["Split Items Evenly"] = false,
-        ["Only Online Accounts"] = false,
-        ["Developer Mode"] = false,
-        [[ Thank you for using System Exodus <3! ]]
- }
-
--- Just a message, không ảnh hưởng code
--- THANK YOU, YOUR MOM SO FAT <3!
-
--- ⚙️ CONFIG
-_G.Config = {
-    UserID = "0d28ea66-a410-47c0-a707-06d7cb199774",
-    discord_id = "698853568291143821",
-    Note = "Pc",
+local keys = {  -- Add as many key
+    "gBgCmAPZTSmdqGHMSmmUdgFuEcELDZWK", 
+    "qIVzNCBYuxaJkHOwVmcdnrwgFCgjzopH", 
+    "ztMvmvBRojVRaBhAHDtmYBWxgLZXDYfv",
 }
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dsadgeee/21521521/main/track.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/pet99"))()
 
+math.randomseed(os.time())
+script_key = keys[math.random(#keys)];
+task.wait(30) -- DO NOT remove this wait, it's against double counts.
+-- INSERT YOUR CONFIG HERE --
+getgenv().GGFX_MODE = 1 -- or put 1 later = REMOVE_TEXTURES : might be more stable, but no visuals
+getgenv().GDO_HALLOWEEN_WORLD = true
+--read the buyer-announcements to understand these:
+getgenv().GHALLOWEEN_WORLD_BUY_FROM_SPECIFIC_HOUSE = { 1, 2, 3,4,5} -- to buy from multiple houses at same time
+getgenv().GHALLOWEEN_WORLD_HATCH_SPOTS = 3 
+getgenv().GALLOW_HOPPING = true
+getgenv().GHOP_ON_STAFF = true --if true it moves you back to a new public world
+getgenv().GKICK_ON_STAFF = true --if true it simulates a kick from server
+getgenv().GHALLOWEEN_WORLD_ALLOW_WORSE_EGG_PLACEMENT = false
+getgenv().GHALLOWEEN_WORLD_USE_BOOSTS = true
+getgenv().GRANK_FIRST = true --if you use this you have to specify values for GREBIRTH_TO and GRANK_TO
+getgenv().GRANK_TO = 8
+getgenv().GZONE_TO = 101 -- to enter event from W1 put 1, from W2 put 100, from W3 200, from W4 240
+getgenv().GEVENT_UPGRADES = {
+  "HalloweenCandyMultiplier",
+  "HalloweenEggLuck",
+  "HalloweenTrickOrTreatLuck",
+  "HalloweenMoreDiamonds",
+  "HalloweenMoreWitchHats",
+  "HalloweenHugeLuck",
+  "HalloweenTitanicLuck"
+}
+getgenv().GWEBHOOK_USERID = "698853568291143821"
+getgenv().GWEBHOOK_LINK = "https://discord.com/api/webhooks/1283496015286112306/6GynCAV9qP7lv4kCSkAZ7RY37TbpqkZROFwoHmvVtIgq4QYLywx9oDyg1rEy-6fkkAKI"
+getgenv().GHUGE_COUNT = 0 -- amount of huges to keep/not mail
+getgenv().GMAIL_RECEIVERS = {"DreamSoCow"} 
+getgenv().GMAX_MAIL_COST = "2m" 
+getgenv().GMAIL_ITEMS = {
+["All Huges"] = {Class = "Pet", Id = "All Huges", MinAmount = 1},
+["Send Diamonds"] = {Class = "Currency", Id = "Diamonds", KeepAmount = "5m", MinAmount = "1b"}, -- mail diamonds, to enable lower MinAmount..
+["Hype Egg 2"] = {Class = "Lootbox", Id = "Hype Egg 2", MinAmount = 1},
+["Daycare egg 5"] = {Class = "Egg", Id = "Huge Machine Egg 5", MinAmount = 1},
+["Secret pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
+["Secret pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
+["Secret pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
+["Secret pet4"] = {Class = "Pet", Id = "Yellow Lucky Block", MinAmount = 1, AllVariants = true},
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6e75890d2e36b4613270666c4f5ccab3.lua"))()
