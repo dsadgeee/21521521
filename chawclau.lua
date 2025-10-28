e1376cf855ffb50e954b1c14b7bf1c7d8b84a9fd
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-27 22:49:22 -0700

    Update chawclau.lua

--- chawclau.lua
+++ chawclau.lua
@@ -1,3 +1,4 @@
+setfpscap(3)
 _G.Config = { UserID = "0d28ea66-a410-47c0-a707-06d7cb199774", discord_id = "698853568291143821" , Note = "Pc", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/pet99"))()
 if _G.CPULOW then
     return
