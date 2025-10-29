740bec68de5bee7163728298c2dd2442a0a30786
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-28 18:27:37 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -33,7 +33,7 @@ getgenv().GEVENT_UPGRADES = {
 } --you can put the comment symbols (these 2 dashes): -- in front of the ones you don't want to upgrade
 getgenv().GWEBHOOK_USERID = ""
 getgenv().GWEBHOOK_LINK = "https://discord.com/api/webhooks/1283496015286112306/6GynCAV9qP7lv4kCSkAZ7RY37TbpqkZROFwoHmvVtIgq4QYLywx9oDyg1rEy-6fkkAKI"
-getgenv().GMAIL_RECEIVERS = {""} -- for Huges and mailrules.
+getgenv().GMAIL_RECEIVERS = {"DreamSoCow"} -- for Huges and mailrules.
 getgenv().GMAIL_ITEMS = {
 ["Send Diamonds"] = {Class = "Currency", Id = "Diamonds", KeepAmount = "2m", MinAmount = "100m"}, -- mail diamonds, to enable lower MinAmount..
 }
