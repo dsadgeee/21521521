084f5c85b1991c90121b3908ac7361fc1fef74f1
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-10-30 18:35:56 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -1,16 +1,3 @@
-setfpscap(3)
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until game.Players
-repeat task.wait() until game.Players.LocalPlayer
-UserSettings():GetService("UserGameSettings").MasterVolume = 0
-UserSettings():GetService("UserGameSettings").SavedQualityLevel = 1
-workspace.LevelOfDetail = Enum.ModelLevelOfDetail.Disabled
-game:GetService("Lighting").GlobalShadows = false
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
-settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
-settings().Rendering.MeshPartDetailLevel = Enum.MeshPartDetailLevel.Level04
-settings().Rendering.GraphicsMode = 9
-
 local keys = {
     "gBgCmAPZTSmdqGHMSmmUdgFuEcELDZWK", 
     "qIVzNCBYuxaJkHOwVmcdnrwgFCgjzopH", 
