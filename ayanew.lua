f76509e8a40b7d7e21c9f757e1fdde0a93efd837
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-11-01 11:32:12 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -1,10 +1,11 @@
 getgenv().Config = {
     DPS_THRESHOLDS = {
-        HOUSE1 = { '>0' },
-        HOUSE2 = { '>1000' },
-        HOUSE3 = { '6000~14000', '>25000' },
-        HOUSE4 = { '14000~20000' },
-        HOUSE5 = { '>24000' },
+        HOUSE1 = { '>0' }, -- mua theo dps , buy by dps
+        HOUSE2 = { '>1000' }, 
+        HOUSE3 = { '6000~14000', '>25000' }, 
+        HOUSE4 = { '14000~20000' }, 
+        HOUSE5 = { '>24000' }, 
+        HOUSE6 = { '>30000' },
     },
     plant = {
         PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, -- slot dành cho pet
