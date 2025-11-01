5fc0fb071318a5dab0387cb2b9180f03716905df
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-11-01 14:29:31 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -5,26 +5,27 @@ getgenv().Config = {
         HOUSE3 = { '6000~14000', '>25000' }, 
         HOUSE4 = { '14000~20000' }, 
         HOUSE5 = { '>24000' }, 
-        HOUSE6 = { '>30000' },
+        HOUSE6 = { '>30000' }, 
     },
     plant = {
-        PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, -- slot dành cho pet
-        EGG_SLOTS = { 10 }, -- slot dành cho trứng
+        PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, -- slot đặt pet, put pet
+        EGG_SLOTS = { 10 }, -- slot dành cho trứng -- slot đặt trứng , put egg
     },
-    -- Gửi PET Titanic/Huge
     SEND_PET = {
-        Usernames = { 'TunTrapBoy' },
-        PetSendInterval = 100,
+        Usernames = { 'TunTrapBoy' },  --gửi cho nhiều usename,send multi 'usename'
+        PetSendInterval = 20,
     },
-    -- Gửi DIAMONDS
     SEND_DIAMONDS = {
-        Usernames = { 'DreamSoCow' },
+        Usernames = { 'DreamSoCow' },--gửi cho nhiều usename,send multi usename
         MinDiamonds = 50000000,
     },
-    -- Gửi ITEM
     SEND_ITEM = {
+        Usernames = { 'usename' },
+        ['Spectral Potion'] = { amount = 3 },--gửi cho nhiều usename,send multi usename
+    },
+    SEND_EGGS = {
         Usernames = { 'TunTrapBoy' },
-        ['Exclusive Brainrot Egg'] = { amount = 'all' },
+        ['Exclusive Egg 56'] = true, --- SEND Exclusive Egg
     },
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dsadgeee/21521521/refs/heads/main/track.lua"))()
