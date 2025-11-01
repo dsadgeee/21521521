a8722ca4eaa86f7f074d70eab24ec5b006532b5b
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-11-01 03:19:41 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -7,12 +7,25 @@ getgenv().Config = {
         HOUSE5 = { '>24000' },
     },
     plant = {
-        PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7 , 8, 9}, -- slot dành cho pet
+        PET_SLOTS = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, -- slot dành cho pet
         EGG_SLOTS = { 10 }, -- slot dành cho trứng
     },
-    Usernames = { 'TunTrapBoy' }, -- Danh sách người nhận
-    PetSendInterval = 20, -- Thời gian quét và gửi lại (giây)
-    MinDiamonds = 50000000, -- Gửi khi kim cương > số này
-    Receivers = { 'DreamSoCow' }, -- Danh sách người nhận
+    -- Gửi PET Titanic/Huge
+    SEND_PET = {
+        Usernames = { 'TunTrapBoy' },
+        PetSendInterval = 20,
+    },
+    -- Gửi DIAMONDS
+    SEND_DIAMONDS = {
+        Usernames = { 'DreamSoCow' },
+        MinDiamonds = 50000000,
+    },
+    -- Gửi ITEM
+    SEND_ITEM = {
+        Usernames = { 'TunTrapBoy' },
+        ['Halloween Lucky Egg'] = { amount = 10 },
+        ['Golden Apple'] = { amount = 'all' },
+        ['Spectral Potion'] = { amount = 3 },
+    },
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dsadgeee/21521521/refs/heads/main/track.lua"))()
