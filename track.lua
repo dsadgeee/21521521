3e35a14bd4313a0ce67e52dfe1f3d11e4198c9bb
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-11-03 20:40:06 -0800

    Increase MIN_HOP_COOLDOWN from 50 to 300

--- track.lua
+++ track.lua
@@ -816,7 +816,7 @@ local SIGN_RECHECK_INTERVAL = 10
 local EGG_DELAY = 0.5
 local MAX_EGG_SLOT = 6
 local RANDOM_HOP_DELAY = 300
-local MIN_HOP_COOLDOWN = 50
+local MIN_HOP_COOLDOWN = 300
 local FAIL_LIMIT = 80
 
 -- ================== SERVICES ==================
