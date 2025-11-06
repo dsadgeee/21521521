7425a9c993e2dc8cc38eac9fba0584b71f077ffc
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-11-06 04:30:14 -0800

    Update track.lua

--- track.lua
+++ track.lua
@@ -811,7 +811,7 @@ end)
 
 -- ================== SETTINGS ==================
 local HOUSE_DELAYS =
-    { [1] = 0.1, [2] = 0.1, [3] = 0.5, [4] = 10, [5] = 100, [6] = 200 }
+    { [1] = 0.1, [2] = 0.1, [3] = 0.5, [4] = 20, [5] = 120, [6] = 200 }
 local SIGN_RECHECK_INTERVAL = 10
 local EGG_DELAY = 0.5
 local MAX_EGG_SLOT = 6
