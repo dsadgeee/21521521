0112c540be3790517c2929c2d0d7298ada91e3e2
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-11-12 00:52:07 -0800

    Create so cp rit.lua

---
+++ so cp rit.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpyBeta.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nuIIism/scripts/main/dex.lua"))()
