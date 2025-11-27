f2e224dc7d2b4d5d9f3e9a407a96be8d196adedc
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2025-11-27 09:27:01 -0800

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -32,6 +32,6 @@ getgenv().Config = {
         ['Exclusive Egg 57'] = true,
     },
         AutoSell = true,       -- Bật/tắt auto sell
-        DelayMinutes = 600
+        DelayMinutes = 200
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dsadgeee/21521521/refs/heads/main/track.lua"))()
