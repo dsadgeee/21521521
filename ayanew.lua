79fc015860dc7b8777c6645b0304aab11495d393
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2026-05-10 08:21:13 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -5,9 +5,6 @@ getgenv().Config =  {
         ["URL"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
         ["Discord Id to ping"] = {"993062998652493824"},
         ["Send Normal Huges"] = true,
-
-        ["Rank Updates"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
-        ["Rank Update Min"] = 20 -- will send rank update when reaches that ranl
     },
     ["Mail Settings"] = {
         ["User"] = "DreamSoCow",
@@ -18,6 +15,8 @@ getgenv().Config =  {
         ["Mailing"] = {
             ["Exclusive Special Ops Egg"] = {Class = "Egg", MinAmount = 1},
             ["Exclusive Origami Egg"] = {Class = "Egg", MinAmount = 1},
+            ["Mystical Kitsune"] = {Class = "Pet", MinAmount = 1},
+            ["Wisp Owl"] = {Class = "Pet", MinAmount = 1},
             ["Gift Bag"] = {Class = "Misc", MinAmount = 99},
             ["Diamond Gift Bag"] = {Class = "Misc", MinAmount = 99},
             ["Large Gift Bag"] = {Class = "Misc", MinAmount = 99},
@@ -47,30 +46,9 @@ getgenv().Config =  {
             ["Overload Charm"] = {Class = "Misc", MinAmount = 1},
         }
     },
-    ["Auto Enchant"] = {"Treasure Hunter", "Coins", "Diamonds", "Criticals", "Strong Pets"},
-    ["Time Trial"] = {
-        ["Disabled"] = false, -- will disable Timetrial
-        ["Open Chest"] = 3, -- 1,2,3 or do false to not open
-    },
-
-    ["CPU Saver"] = true, -- makes your game run smoother
-
-    ["MaxEggSlots"] = 99,
-    ["MaxPetSlots"] = 99,
-
-    ["Ignore Do Ranks"] = false, -- wont do quest at all
-
-    ["Fast Rankup"] = false, -- will focus on quest in world 1 until it reaches a specific rank (broken a bit)
-    ["Fast Rankup to"] = 20, -- choose your rank you want to do
-    ["UltraMastery"] = {"Economy", "Eggs", "Potions", "Enchants", "Fruits"},
-
-    ["StarterPets"] = true, -- will hatch event pets
-    ["ClaimFreeStuff"] = false, -- disables claiming stuff loop (may fix stuck on upgrading stuff)
-    ["AutoCombinePresents"] = true, -- combines Prestes to the desired level
-    ["MaxCombineTier"] = 4, -- 1 = max Medium, 2 = max Large, 3 = max X-Large, 4 = max Titanic (default, craft everything)
-    ["OpenPresent"] = 5, -- 1 = small, 2 = Medium, 3 = Large, 4 = X-Large, 5 = Titanic, false = disabled
-
-    ["AutoConsumeFlag"] = {"Strength Flag","Hasty Flag"},
-    ["AutoSprinkler"] = true,
+    ["CPU Saver"] = true,
+    ["AutoUseDice"] = {"Mega Lucky Dice V2"},
+    ["SyncOnWeather"] = false,
+    ["PetsToSell"] = {"Sand Turtle", "Sprout Stag", "Blossom Fox", "Honey Bee", "Chamelon", "Tiedye Elephant", "Surge Wolf", "Starry Eye Wolf", "Bubble Bunny", "Sapphire Dragon", "Wisp Owl", "Mystical Kitsune"},
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/cd6279ef22ef689bec5cd78c7cd2f0a1.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9f9b656e577ba1b9d87d4c855e7ac1a4.lua"))()
