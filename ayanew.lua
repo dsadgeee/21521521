95d63bf39e6fc33260d25d0de3fc3934cd7d7751
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2026-05-12 07:13:44 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -1,4 +1,4 @@
-setfpscap(5)
+setfpscap(3)
 script_key = "kgeosiEkeGGlNyPcCwWryYnIgpVlTcDj";
 getgenv().GGFX_MODE = 1
 getgenv().GZONE_TO = 1 -- to enter event from W1 put 1, from W2 put 100, from W3 200, from W4 240
