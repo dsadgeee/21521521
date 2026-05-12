6f2968779580b1fd83ad1de387145d0dd249bfce
Tgavb <102312281+dsadgeee@users.noreply.github.com>
2026-05-12 09:56:45 -0700

    Update ayanew.lua

--- ayanew.lua
+++ ayanew.lua
@@ -1,38 +1,24 @@
-task.wait(10)
-getgenv().Config = {
-    ["PetPen"] = {
-        ["EggToBuy"] = "Cracked Egg",
-    },
-    ["PetReleaser"] = {
-        ["RaritiesToMakeNeon"] = {"common", "uncommon", "rare", "ultra_rare", "legendary"},
-        ["RaritiesToMakeMega"] = {},
-
-        ["RaritiesToRelease"] = {"common", "uncommon", "rare", "ultra_rare", "legendary"}
-    },
-    ["AutoTrade"] = {
-        ["TradeUsers"] = {
-            ""
-        },
-        ["Pets"] = {
-             "Silverback Gorilla","Balloon Unicorn","Purrowl", "Dragonfruit Fox", "Dango Penguins", "Emberlight", "Frostbite Bear", "Diamond Unicorn", "Sea Turtle", "Glormy Crab"
-        }
-    },
-    ["AutoChangeConfig"] = {
-        ["CrystalEggAmount"] = 1000000000,
-        ["TradeablePets"] = 1000000000,
-        ["CollectServers"] = {
-            "",
-        }
-    },
-    ["BuyItemWithTickets"] = "Crystal Egg",
-    ["OpenCrystalEggs"] = true,
-    ["ConvertBucksToTickets"] = true,
-    ["AutoBuyJiggly"] = true,
-    ["AutoBuyMochi"] = true,
-    ["UseAgeUpPotions"] = true,
-    ["OpenAdminAbuseEggs"] = true,
-    ["KeepTickets"] = false,
-
-    ["WebhookURL"] = "",
+setfpscap(3)
+script_key = "kgeosiEkeGGlNyPcCwWryYnIgpVlTcDj";
+getgenv().GZONE_TO = 1 -- to enter event from W1 put 1, from W2 put 100, from W3 200, from W4 240
+getgenv().GDO_RNG_INSTANCE = true
+getgenv().GUSE_BOOSTS = true
+getgenv().GRNG_SELL_PETS = true
+getgenv().GRNG_CRAFT_DICE = true
+getgenv().GRNG_USE_MEGA1 = true
+getgenv().GEVENT_UPGRADES = {
+    { Id = "RNGEggLuck",   MaxTier = 15  },
+    { Id = "RNGHatchSpeed", MaxTier = 15  },
+    { Id = "RNGBonusLuck",  MaxTier = 8  },
+    { Id = "RNGHugeLuck",   MaxTier = 13  },
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9af1f22af0378cb275bfeac9c6971ba5.lua"))()
+getgenv().GWEBHOOK_USERID = "698853568291143821"
+getgenv().GWEBHOOK_LINK = "https://discord.com/api/webhooks/1408866660017766500/8OgtYYAinAJ9qhpqLxr_jIzUztGg0YqnxzayJql_zAyaYqzoXxUH2OhEoo9DX09HpDOF"
+getgenv().GHUGE_COUNT = 0 -- amount of huges to keep/not mail
+getgenv().GMAIL_RECEIVERS = {"your-receiver-mule"}
+getgenv().GMAIL_ITEMS = {
+["All Huges"] = {Class = "Pet", Id = "All Huges", MinAmount = 1},
+["Send Diamonds"] = {Class = "Currency", Id = "Diamonds", KeepAmount = "5m", MinAmount = "50m"}, -- mail diamonds, to enable lower MinAmount..
+}
+getgenv().GMAX_MAIL_COST = "2m"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
